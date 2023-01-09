64a9cce150ffa66cc90067ca0fc29dde256366f5
Ruslan Bogdanov <russian.bogd@gmail.com>
2023-01-09 11:43:47 +0300

    feat: started to make a project where I will post work on react

---
+++ my-react/src/App.js
@@ -0,0 +1,45 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className={"a1"}>
+        <div>
+          <h1 className="b3">Ruslan Bogdanov ✌</h1>
+        </div>
+        <div>
+          <div className="b2"></div>
+        </div>
+        <div className="b4">some stupid text</div>
+      </header>
+      <main>
+        <section className={"a2"}>
+          <div className="x1">
+            <div className="c1">
+              <p>React</p>
+            </div>
+          </div>
+          <div>
+            <p className="c2">
+              super duper stupid text, super duper stupid text, super duper
+              stupid text, super duper stupid text
+            </p>
+          </div>
+        </section>
+        <section className={"a3"}>
+          <div className={"b1"}>1</div>
+          <div className={"b1"}>1</div>
+          <div className={"b1"}>1</div>
+          <div className={"b1"}>1</div>
+          <div className={"b1"}>1</div>
+          <div className={"b1"}>1</div>
+          <div className={"b1"}>1</div>
+          <div className={"b1"}>1</div>
+        </section>
+      </main>
+      <footer></footer>
+    </div>
+  );
+}
+
+export default App;
