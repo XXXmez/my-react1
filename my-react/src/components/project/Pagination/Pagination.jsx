351dbcff4481135d0509c492653fe1a0356ea445
Ruslan Bogdanov <russian.bogd@gmail.com>
2023-01-10 16:28:49 +0300

    feat: added spinner when loading content from api

--- my-react/src/components/project/Pagination/Pagination.jsx
+++ my-react/src/components/project/Pagination/Pagination.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Items from "./Items";
 import NavPages from "./NavPages";
+import { Oval } from "react-loader-spinner";
 import "./Pagination.css";
 
 const Pagination = ({ urlBase = "" }) => {
@@ -21,19 +22,35 @@ const Pagination = ({ urlBase = "" }) => {
   }, []);
 
   return (
-    <div className="paginations" style={loading ? { opacity: "0.5" } : {}}>
-      <Items
-        itemsBase={recordBase}
-        currentPage={currentPage}
-        countItems={countItems}
-      />
-
-      <NavPages
-        setCurrentPage={setCurrentPage}
-        currentPage={currentPage}
-        baseLength={recordBase.length}
-        countItems={countItems}
-      />
+    <div className="paginations">
+      {loading ? (
+        <Oval
+          height={80}
+          width={80}
+          color="#4fa94d"
+          wrapperStyle={{}}
+          wrapperClass=""
+          visible={true}
+          ariaLabel="oval-loading"
+          secondaryColor="#4fa94d"
+          strokeWidth={2}
+          strokeWidthSecondary={2}
+        />
+      ) : (
+        <>
+          <Items
+            itemsBase={recordBase}
+            currentPage={currentPage}
+            countItems={countItems}
+          />
+          <NavPages
+            setCurrentPage={setCurrentPage}
+            currentPage={currentPage}
+            baseLength={recordBase.length}
+            countItems={countItems}
+          />
+        </>
+      )}
     </div>
   );
 };
